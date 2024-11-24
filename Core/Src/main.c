57d2f6f05aee2e1ad5924afc81a22f72e3af0aa4
0xBB8 <lampmud@163.com>
2024-11-24 21:58:12 +0800

    smooth animation

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -105,7 +105,7 @@ int main(void) {
             SH1106_Fill(Black);
             SH1106_DrawBitmap(0, 0, chiikawaallArray[i], 128, 64, White);
             HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-            HAL_Delay(69);
+            HAL_Delay(20);
             SH1106_UpdateScreen();
             printf("this is the NO. %d frame for chiikawaa\r\n", i);
         }
